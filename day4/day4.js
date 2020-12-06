a50598c6778e0ca9aba6f3ee0493715c5f123dd8
Nir Weber <webernir@tipranks.com>
2020-12-06 10:17:23 +0200

    Day 4 - Part B - Solved

--- day4/day4.js
+++ day4/day4.js
@@ -13,7 +13,7 @@ function lineToPassport(line) {
 
 const requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]; // optional cid
 
-function isValid(item) {
+function hasAllRequiredFields(item) {
   const keys = Object.keys(item);
   const missing = requiredFields.filter((field) => !keys.includes(field));
   return missing.length === 0;
@@ -26,9 +26,68 @@ function partA() {
 
   const lines = txt.split("\r\n\r\n");
 
-  const fixedLines = lines.map(fixLine).map(lineToPassport).filter(isValid);
+  const fixedLines = lines
+    .map(fixLine)
+    .map(lineToPassport)
+    .filter(hasAllRequiredFields);
 
   return fixedLines;
 }
 
-console.log(partA().length);
+function isValidHeight(val) {
+  if (val.includes("cm")) {
+    const num = Number(val.replace("cm", "").trim());
+    return num >= 150 && num <= 193;
+  }
+  if (val.includes("in")) {
+    const num = Number(val.replace("in", "").trim());
+    return num >= 59 && num <= 76;
+  }
+  return false;
+}
+
+const validEcls = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+function isValidEcl(val) {
+  return validEcls.includes(val);
+}
+
+const validators = {
+  byr: (val) => val >= 1920 && val <= 2002,
+  iyr: (val) => val >= 2010 && val <= 2020,
+  eyr: (val) => val >= 2020 && val <= 2030,
+  hgt: isValidHeight,
+  hcl: (val) => /#([0-9a-f]){6}/.test(val),
+  ecl: isValidEcl,
+  pid: (val) => /^\d{9}$/.test(val),
+  pidOld: (val) => /\d{9}/.test(val),
+  cid: () => true,
+};
+
+function isValid(item) {
+  console.log(item);
+  return (
+    Object.keys(item).filter((k) => {
+      const isKeyValid = validators[k](item[k]);
+      console.log(k, item[k], isKeyValid);
+      return !isKeyValid;
+    }).length == 0
+  );
+}
+
+function partB() {
+  const txt = fs.readFileSync(path.join(__dirname, "./input.txt"), "utf8");
+
+  const lines = txt.split("\r\n\r\n");
+
+  const fixedLines = lines
+    .map(fixLine)
+    .map(lineToPassport)
+    .filter(hasAllRequiredFields)
+    .filter(isValid);
+
+  return fixedLines;
+}
+
+// console.log(partA().length);
+console.log(partB().length);
